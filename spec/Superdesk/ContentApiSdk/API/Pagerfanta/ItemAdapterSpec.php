c63aed7170ea4835e045677ff1a020d7a56fb4f9
Mischa Gorinskat <m.gorinskat@gmail.com>
2015-12-08 14:34:13 +0100

    Test commit again

--- spec/Superdesk/ContentApiSdk/API/Pagerfanta/ItemAdapterSpec.php
+++ spec/Superdesk/ContentApiSdk/API/Pagerfanta/ItemAdapterSpec.php
@@ -30,13 +30,14 @@ class ItemAdapterSpec extends ObjectBehavior
         $this->shouldHaveType('Superdesk\ContentApiSdk\API\Pagerfanta\ItemAdapter');
     }
 
-    function let(ClientInterface $client, PaginationDecorator $paginationRequest)
+    function let(ClientInterface $client, Request $request)
     {
-        $this->beConstructedWith($client, $paginationRequest);
+        $this->beConstructedWith($client, $request);
     }
 
-    function its_method_get_slice_should_return_items($client, $paginationRequest)
+    function its_method_get_slice_should_return_items($client, $request)
     {
+        $paginationRequest = new PaginationDecorator($request->getWrappedObject());
         $paginationRequest->addPagination(1, 1);
         $response = new Response('{"_links": {"self": {"title": "items", "href": "items?start_date=2015-01-01&max_results=2&page=1"}, "parent": {"title": "home", "href": "/"}}, "_meta": {"total": 2, "max_results": 2, "page": 1}, "_items": [{"headline": "Andromeda on a collision course", "body_text": "Andromeda and Milky Way will collide in about 2 billion years", "_links": {"self": {"title": "Item", "href": "items/tag:example.com,0001:newsml_BRE9A607"}}, "versioncreated": "2015-03-09T16:32:23+0000", "pubstatus": "usable", "version": "2", "uri": "http://api.master.dev.superdesk.org/items/tag%3Aexample.com%2C0001%3Anewsml_BRE9A607", "_type": "items", "language": "en", "type": "text"}, {"headline": "Die Lore-Ley", "body_text": "Ich wei\u00df nicht was soll es bedeuten", "_links": {"self": {"title": "Item", "href": "items/tag:example.com,0001:newsml_BRE9A608"}}, "versioncreated": "2015-04-19T13:45:54+0000", "pubstatus": "usable", "version": "3", "uri": "http://api.master.dev.superdesk.org/items/tag%3Aexample.com%2C0001%3Anewsml_BRE9A608", "_type": "items", "language": "de", "type": "text"}]}');
         $client->makeApiCall($paginationRequest)->shouldBeCalled()->willReturn($response);
